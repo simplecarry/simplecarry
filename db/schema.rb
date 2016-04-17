8b1f05040cbcd301d07bdc2b5dbaeb0532e5997e
HieuNM6 <hieunmce@gmail.com>
2016-04-17 18:06:34 +0700

    enhancement comment ui

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417090128) do
+ActiveRecord::Schema.define(version: 20160417102204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,19 +109,19 @@ ActiveRecord::Schema.define(version: 20160417090128) do
   add_index "travel_plans", ["user_id"], name: "index_travel_plans_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",                                                 null: false
+    t.string   "encrypted_password",     default: "",                                                 null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,                                                  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "avatar"
+    t.datetime "created_at",                                                                          null: false
+    t.datetime "updated_at",                                                                          null: false
+    t.string   "avatar",                 default: "http://ssl.gstatic.com/accounts/ui/avatar_2x.png"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
