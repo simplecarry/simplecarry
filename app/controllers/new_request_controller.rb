7203a46e777be25d7b702eff56f48f495abac10d
Kha Nguyen <kha@tinypulse.com>
2016-04-17 16:24:51 +0700

    notification on new request

--- app/controllers/new_request_controller.rb
+++ app/controllers/new_request_controller.rb
@@ -1,48 +1,56 @@
 class NewRequestController < ApplicationController
   include Wicked::Wizard
   steps :item, :location, :price
-  
+
   def show
     case step
-    when :item
-      @request = Request.new
-      session[:request] = nil
-    else
-      @request = Request.new(session[:request])
+      when :item
+        @request = Request.new
+        session[:request] = nil
+      else
+        @request = Request.new(session[:request])
     end
     render_wizard
   end
 
   def update
     case step
-    when :item
-      @request = current_user.requests.new(request_params)
-      session[:request] = @request.attributes
-      @request.check_validate = step
-      if @request.valid?
+      when :item
+        @request = current_user.requests.new(request_params)
+        session[:request] = @request.attributes
+        @request.check_validate = step
+        if @request.valid?
+          redirect_to next_wizard_path
+        else
+          render_wizard
+        end
+      when :location
+        session[:request] = session[:request].merge(params[:request])
         redirect_to next_wizard_path
-      else
-        render_wizard
-      end
-    when :location
-      session[:request] =  session[:request].merge(params[:request])
-      redirect_to next_wizard_path
-    when :price
-      session[:request] = session[:request].merge(params[:request])
-      @request = current_user.requests.new(session[:request])
-      @request.check_validate = "active"
-      @request.status = :open
-      if @request.save
-        redirect_to root_path
-      else
-        render_wizard
-      end
+      when :price
+        session[:request] = session[:request].merge(params[:request])
+        @request = current_user.requests.new(session[:request])
+        @request.check_validate = "active"
+        @request.status = :open
+        if @request.save
+          send_new_request_notification
+          redirect_to root_path
+        else
+          render_wizard
+        end
     end
   end
 
   private
-    def request_params
-      params.require(:request).permit!
-    end
+  def request_params
+    params.require(:request).permit!
+  end
+
+  def send_new_request_notification
+    NewOfferNotification.create(
+        receiver_id: @request.requester_id,
+        content: "You created a new request <a href='#{request_url(@request)}'>##{@request.id}</a>",
+        event_type: Request.statuses[:open])
+  end
 
 end
