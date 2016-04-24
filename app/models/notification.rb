10fc1481e08d5d62c84a8efa0f12fac229432787
Kha Nguyen <kha@tinypulse.com>
2016-04-24 16:24:46 +0700

    sort order

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ActiveRecord::Base
   def self.user_inbox(user, event_type = nil)
-    query = Notification.where(receiver_id: user.id)
+    query = Notification.where(receiver_id: user.id).order('created_at DESC')
     if event_type
       query = query.where(event_type: event_type)
     end
