2ced9c24764c0d52c51d729ed673990d702d5f9d
Huy T <td.huycan@gmail.com>
2016-04-24 13:02:55 +0700

    fix flash for comment socket

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,24 @@ class CommentsController < ApplicationController
     @request = Request.find params[:comment][:request_id]
     if @comment.save
       respond_to do |format|
-        format.js
+        format.js {
+          flash[:success] = "Your comment is created"
+        }
         format.html {
           flash[:success] = "Your comment is created"
           redirect_to request_path(@request)
         }
       end
     else
+      respond_to do |format|
+        format.js {
+          flash[:error] = "Your comment is too short (minimum is 5 characters)"
+        }
+        format.html {
           flash[:error] = "Your comment is too short (minimum is 5 characters)"
           redirect_to request_path(@request)
+        }
+      end
     end
   end
   private
